960950207894753878e31e3576a61a8c7657c248
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-09-02 01:38:58 +0900

    Extract StockItemForm

--- lib/components/pages/add_stock_item.dart
+++ lib/components/pages/add_stock_item.dart
@@ -1,5 +1,6 @@
-import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+
+import './add_stock_item/stock_item_form.dart';
 
 class AddStockItem extends StatelessWidget {
   const AddStockItem({Key? key}) : super(key: key);
@@ -10,44 +11,7 @@ class AddStockItem extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add Stock Item'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Padding(
-                padding: EdgeInsets.all(8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Item',
-                  ),
-                )),
-            const Padding(
-                padding: EdgeInsets.all(8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Amount',
-                  ),
-                  keyboardType: TextInputType.number,
-                )),
-            Padding(
-                padding: const EdgeInsets.all(8),
-                child: DateTimePicker(
-                  type: DateTimePickerType.date,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Best Before',
-                  ),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                )),
-            Expanded(child: Container()),
-            ElevatedButton(onPressed: () {}, child: const Text('Add'))
-          ],
-        ),
-      ),
+      body: const Center(child: StockItemForm()),
     );
   }
 }
---
+++ lib/components/pages/add_stock_item/stock_item_form.dart
@@ -0,0 +1,89 @@
+import 'package:date_time_picker/date_time_picker.dart';
+import 'package:flutter/material.dart';
+
+import 'package:home_assistant/stock.dart';
+
+class StockItemForm extends StatefulWidget {
+  final void Function(
+      {required String itemName,
+      required Amount amount,
+      required DateTime bestBefore})? onSubmit;
+
+  const StockItemForm({super.key, this.onSubmit});
+
+  @override
+  State<StockItemForm> createState() => _StockItemFormState();
+}
+
+class _StockItemFormState extends State<StockItemForm> {
+  String _itemName = '';
+  Amount? _amount;
+  DateTime? _bestBefore;
+
+  @override
+  Widget build(BuildContext context) {
+    final allValuesSet =
+        _itemName.isNotEmpty && _amount != null && _bestBefore != null;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Item',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _itemName = value;
+                });
+              },
+            )),
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Amount',
+                  suffixText: 'g'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  _amount = Amount(double.parse(value), Unit.gram);
+                });
+              },
+            )),
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: DateTimePicker(
+              type: DateTimePickerType.date,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Best Before',
+              ),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+              onChanged: (value) {
+                setState(() {
+                  _bestBefore = DateTime.parse(value);
+                });
+              },
+            )),
+        Expanded(child: Container()),
+        ElevatedButton(
+            onPressed: allValuesSet ? _submitValues : null,
+            child: const Text('Add'))
+      ],
+    );
+  }
+
+  void _submitValues() {
+    if (widget.onSubmit != null) {
+      widget.onSubmit!(
+          itemName: _itemName, amount: _amount!, bestBefore: _bestBefore!);
+    }
+  }
+}
