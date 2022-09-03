f28bc616dcbc9df13e890dcb468f46f5fa6a790c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-09-03 17:17:33 +0900

    Find directory once during initialization
    So the getter can still be sync

--- lib/infra/local_json_file_stock_item_repository.dart
+++ lib/infra/local_json_file_stock_item_repository.dart
@@ -8,24 +8,25 @@ import 'package:home_assistant/domain/stock.dart';
 
 class LocalJSONFileStockItemRepository extends StockItemRepository {
   final String filename;
-  Iterable<StockItem> _values = [];
+  late final File _file;
 
   LocalJSONFileStockItemRepository(this.filename);
 
   @override
-  get values => _values;
-
-  Future<void> load() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/$filename');
+  get values {
     List<dynamic> contents = [];
-    if (file.existsSync()) {
-      contents = jsonDecode(file.readAsStringSync());
+    if (_file.existsSync()) {
+      contents = jsonDecode(_file.readAsStringSync());
     }
-    _values = contents.map<StockItem>((item) => StockItem(
+    return contents.map<StockItem>((item) => StockItem(
           name: item['itemName'],
           amount: Amount.parse(item['amount']),
           bestBefore: DateTime.parse(item['bestBefore']),
         ));
+  }
+
+  Future<void> load() async {
+    final directory = await getApplicationDocumentsDirectory();
+    _file = File('${directory.path}/$filename');
   }
 }
